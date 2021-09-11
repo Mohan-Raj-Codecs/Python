3e1343d7b30bc777ae521cc2bbfc402e3038c113
Mohan Raj <Mohan-Raj-Codecs@users.noreply.github.com>
2021-09-11 14:30:43 +0530

    Create Requirements.bat

---
+++ Image Viewer/Requirements.bat
@@ -0,0 +1,73 @@
+@echo off
+title Installing Modules
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                              Python Module Install Batch
+echo.
+echo.
+echo.
+echo                                             	                 --Scripts By Mohan Raj
+echo.
+echo.
+echo.
+echo.
+echo.
+echo Please Wait...........
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+timeout 4
+cls
+color 09
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                        			    Installing Modules......................
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                It will take about 2 Minutes...................
+echo.
+echo.
+echo                                                This Bachelor File can Run on Background Let You Do Other Task :) ....
+echo.
+echo.
+echo                                                Don't Worry About Error While Executing our Commmands................
+echo.
+echo.
+echo                                                Keep Your Internet Connected :) ..................
+timeout 5 
+cls
+py -m pip install pygame tk pillow
+echo                                              Listing Modules
+py -m pip list
+timeout 3
+color 0A
+echo.
+echo.
+echo.
+echo.
+echo                                              Python Modules Installed SuccessFully
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
