4aa10a1d8ee00358f696bf2afcc0c83de5c50ea7
Mohan Raj <74151223+Mohan-Raj-Codecs@users.noreply.github.com>
2021-09-26 20:47:54 +0530

    Update ssh.py

--- Discord Bot/Password_Auth/ssh.py
+++ Discord Bot/Password_Auth/ssh.py
@@ -1,11 +1,11 @@
 import paramiko
 
 Server_on = False
-ip="65.0.139.80"
+ip="ip"
 user="ubuntu"
-passwd=""
-#pem_file="MIRACLE.pem"
-#key_file=paramiko.RSAKey.from_private_key_file(pem_file)
+passwd="password"
+pem_file="MIRACLE.pem"
+key_file=paramiko.RSAKey.from_private_key_file(pem_file)
 ssh_client=paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -14,8 +14,8 @@ def exe(x,pas):
     pas=str(pas)
     cmd=x
     print("Logging in ssh")
-    #ssh_client.connect(hostname=ip,username=user,pkey=key_file)   #Authorization using PEM file
-    ssh_client.connect(hostname=ip,username=user,password=pas)  #Authorization using password
+    ssh_client.connect(hostname=ip,username=user,pkey=key_file)   #Authorization using PEM file
+    #ssh_client.connect(hostname=ip,username=user,password=pas)  #Authorization using password
     print("Logging Successfull")
     print("Executing....")
     stdin,stdout,stderr=ssh_client.exec_command(cmd)
