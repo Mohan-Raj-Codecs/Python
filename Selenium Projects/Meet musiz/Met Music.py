b772e81e79a9b8443b96631d10d7aa49349afada
Mohan Raj <74151223+Mohan-Raj-Codecs@users.noreply.github.com>
2021-11-12 02:10:07 +0530

    Update Met Music.py

--- Selenium Projects/Meet musiz/Met Music.py
+++ Selenium Projects/Meet musiz/Met Music.py
@@ -19,7 +19,7 @@ options=Options()
 options.add_experimental_option("debuggerAddress","localhost:8989")
 driver = webdriver.Chrome(executable_path="chromedriver.exe",options=options)
 actions = ActionChains(driver)
-driver.get("https://meet.google.com/vqm-ynno-wnf")
+driver.get("https://meet.google.com/ion-afbo-uwo")
 ####
 
 
@@ -106,7 +106,7 @@ def Chat_box(force=False):
     try:
         Chat_open(force)
         sleep(1)
-        Chat_close(force)
+        Chat_close()
     except:
         if force:
             print("Researching Chat Box........")
@@ -114,18 +114,21 @@ def Chat_box(force=False):
         else:
             return
 
-def Chat_fetch(force=False):
+def Chat_fetch(bot=None,force=False):
     chats=[]
     try:
         chat = driver.find_elements_by_xpath("//div[@data-message-text]")
+        for i in chat:
+            chats.append(i.get_attribute("data-message-text"))
     except:
         if force:
-            Chat_fetch(force)
+            Chat_fetch(bot,force)
         else:
-            return chats
-    for i in chat:
-        chats.append(i.get_attribute("data-message-text"))
-    return chats
+            pass
+    if bot==None:
+        return chats
+    else:
+        return bot.mod(chats)
 
 def Chat_detail(force=False):
     names=[]
@@ -144,8 +147,31 @@ def Chat_detail(force=False):
         timestamps.append(i.get_attribute("data-timestamp"))
     return names,timestamps
 
-def Chat_send(x):
-    pass
+def Chat_send(x,bot=None,force=False):
+    try:
+        sleep(0.3)
+        Chat_close()
+        sleep(0.3)
+        Chat_open(True)
+        sleep(0.5)
+        sender = WebDriverWait(driver, 1).until(
+            EC.presence_of_element_located((By.XPATH, "//textarea")))
+        #sender.click()
+        sender.send_keys(str(x))
+        sleep(0.05)
+        sender.send_keys(Keys.RETURN)
+        Chat_close()
+        print("Sent Response : ",x)
+        if bot==None:
+            print("Custom Chat ignore")
+        else:
+            bot.add(len(Chat_fetch(None,True))-1)
+    except:
+        if force:
+            Chat_send(x,bot,force)
+        else:
+            return
+    
 ####
 
 def wait_until(func, timeout, period=0.25):
@@ -159,31 +185,76 @@ def wait_until(func, timeout, period=0.25):
         print("Waiting : ",timeout - (mustend - time.time()) )
     return False
 
+class evade:
+    def __init__(self):
+        self.vars=[]
 
+    def add(self,x,first=False):
+        if first:
+            just=[0]
+            just.extend(self.vars)
+            just[0]=x
+            self.vars=just
+        else:
+            self.vars.append(x)
+        print("Added : ",x)
+
+    def rem(self,x,_all=False):
+        if _all:
+            self.vars=list(i for i in self.vars if i!=x)
+        else:
+            self.vars.remove(x)
+
+    def mod(self,x):
+        x=list(x)
+        for i in self.vars: #Assign Dummy
+            try:
+                x[i]="dummy"
+            except IndexError:
+                pass
+##                if i==len(x):
+##                    x.append(0)
+##                x[i]="dummy"
+        x=list(i for i in x if i!="dummy") #Remove Dummy
+        return x
+
+    def unread():
+        pass
+
+    def ret(self):
+        return self.vars
+
+def process_chat(bot,x):
+    #if x in "/": #Do Something Filter
+    Chat_send(str(x),bot,True)
+    
 
 
 Precaution()
 wait_until(func=Join().click,timeout=7)
 Chat_box(force=True)
 
-    
-while True:
-    chats=Chat_fetch(True)
-    names,times=Chat_detail(True)
-    print(len(chats))
-    for i in range(len(chats)):
-        try:
-            print(names[i]," : ",chats[i]," : ",times[i])
-        except IndexError:
-            for j in range(i-1,100):
-                try:
-                    print(names[i-j]," : ",chats[i]," : ",times[i-j])
-                    break
-                except IndexError:
-                    pass
-                
-    sleep(1)
+old_len=0
+bot_cmd = evade()
+Chat_send("Hey This is Groovy , a Music Bot\nvisit : www.groovy.com to know commands",None,True)
+chets=Chat_fetch(None,True)
+bot_cmd.add(len(chets)-1)
+bot_cmd.add(len(chets)-2)
 
+
+while True:
+    chats=Chat_fetch(bot_cmd,True)
+    print(chats)
+    print(Chat_fetch(None,True))
+    if len(chats)>old_len: #Changes detected
+        if len(chats)==old_len+1: #Only one chat came
+            process_chat(bot_cmd,chats[len(chats)-1])
+        else: #many chats came
+            for i in range(old_len,len(chats)):
+                process_chat(bot_cmd,chats[i])
+                
+    old_len=len(chats)
+    sleep(0.5)
 
 
 #/div data-message-text
