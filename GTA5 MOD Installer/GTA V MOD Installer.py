6b058743afb8999409f7adfdbaf314326bc12985
Mohan Raj <74151223+Mohan-Raj-Codecs@users.noreply.github.com>
2021-12-24 15:29:49 +0530

    GTA mod updated

--- GTA5 MOD Installer/GTA V MOD Installer.py
+++ GTA5 MOD Installer/GTA V MOD Installer.py
@@ -2,26 +2,41 @@ from subprocess import call
 from os import getcwd
 from time import sleep
 
-def c(x):
+def cal(x):
     x = str(x)
     call(x, shell=True)
+    
+def de(x):
+    return str("del \""+str(x)+"\" /q /s\n")
+def der(x):
+    return str("RD /q /s \""+str(x)+"\" \n")
 
+def uninstall():
+    
+    uninstall_code = str('@echo off\n'+der("menyooStuff")+der("scripts")+de("ScriptHookVDotNet3.xml")+de("ScriptHookVDotNet3.dll")+de("ScriptHookVDotNet2.xml")+de("ScriptHookVDotNet2.dll")+de("ScriptHookVDotNet.ini")+de("ScriptHookVDotNet.asi")+de("ScriptHookV.dll")+de("NativeTrainer.asi")+de("Menyoo.asi"))
+    misc_files=str(de("exclude.txt")+de("GTA V MOD Installer.py")+de("menyooLog.log")+de("ScriptHookVDotNet.log")+de("ScriptHookV.log")+de("asiloader.log"))
+    openiv_except=str('for %%i in (*.asi) do if not "%%~i" == "OpenIV.asi" del "%%~i" \n')
+    self_destruct=str(de("MOD Uninstaller.bat")+"@echo on")
+        
+    with open('MOD Uninstaller.bat', 'w') as f:
+        f.write(uninstall_code+misc_files+openiv_except+self_destruct)
+    f.close()
 
-def u():
-    abc = '@echo off\ndel Menyoo.asi /q /s\nRD /q /s menyooStuff\ndel ScriptHookV.dll /q /s\ndel NativeTrainer.asi /q /s\ndel dinput8.dll /q /s\ndel "MOD Uninstaller.bat"'
-    with open('MOD Uninstaller.bat', 'w') as (f):
-        f.write(abc)
+    with open('exclude.txt', 'w') as f:
+        f.write(".py\n.txt\n")
     f.close()
 
 
-u()
+uninstall()
 GTAV = input('Enter the path of GTAV : ')
 GTA = GTAV
 GTAV = '"' + GTAV + '"'
 Sou = str(getcwd())
 So = Sou
 Sou = '"' + Sou + '"'
-c('xcopy /E /I /Y *.* ' + GTAV)
-c('color A0')
+cal('xcopy /E /I /Y /EXCLUDE:exclude.txt  * ' + GTAV)
+cal('color A0')
 print('                           MOD Installed :)')
+cal(de('MOD Uninstaller.bat'))
+cal(de('exclude.txt'))
 sleep(5)
--- GTA5 MOD Installer/README.txt
+++
@@ -1,9 +0,0 @@
-This Py Program will Install Menyoo Mods to the GTA V without requiring you do File Operations
-
-How to Use :
-
-1#Copy and paste the GTA V.exe Game Folder Path in Prompt and Press Enter
-
-2#Done It will Copy all the Menyoo MODS to GTAV, Ready to Run
-
-3#You can see a File(MOD Uninstaller.bat) in GTA V path , Double Click on that to Uninstall the MODS
