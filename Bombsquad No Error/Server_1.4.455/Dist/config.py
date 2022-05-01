3bdee79f8390b48475afc935d9dd276e5732c75a
Mohan Raj <Mohan-Raj-Codecs@users.noreply.github.com>
2022-05-01 19:46:46 +0530

    Update config.py

--- Bombsquad No Error/Server_1.4.455/Dist/config.py
+++ Bombsquad No Error/Server_1.4.455/Dist/config.py
@@ -1,8 +1,8 @@
 # place any of your own overrides here.
 # see bombsquad_server for details on what you can override
 # examples (uncomment to use):
-config['partyName'] = u'\ue043 \ue047 HARLEY QUINN EPIC TEAMS \ue047 \ue043'
+config['partyName'] = u'\ue043 \ue047 Guava Plant EPIC TEAMS \ue047 \ue043'
 config['sessionType'] = 'teams'
 config['maxPartySize'] = 15
-config['port'] = 19710
+config['port'] = 43210
 config['playlistCode'] = 319087
